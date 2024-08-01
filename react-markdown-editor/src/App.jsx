aa5cd6932ae205997e27cd2208bd830a1a17da0c
Josh60169 <yashvihangupta@gmail.com>
2024-08-01 01:56:02 -0500

    hopefully implemented Redux correctly

--- react-markdown-editor/src/App.jsx
+++ react-markdown-editor/src/App.jsx
@@ -3,20 +3,51 @@ import {Provider, connect} from 'react-redux';
 import {createStore} from 'redux';
 import {marked} from "https://cdnjs.cloudflare.com/ajax/libs/marked/13.0.2/lib/marked.esm.js";
 
+const CODE_CHANGED = 'CODE CHANGED';
+const defaultCode = "# This is the first header!\n" + 
+      "## This is the second header!\n" +
+      "You can create links with markdown such as this one to YouTube [Click Me!](https://www.youtube.com)\n" +
+      "This is how a line of code is made: `console.log(\'hello world\')`.\n" +
+      "The following is an inline code block:\n" +
+      "```\nfunction showcase() {\n" +
+      "    console.log(\'This is a function!\');\n" +
+      "}\n```\n\n\n" +
+      "You can create blockquotes like this: \n> Here is a blockquote\n\n" +
+      "The text can be **bold**!\n\n" +
+      "1. You can have\n2. an ordered list like this!\n\n- Or it can be\n- an unordered list instead!\n\n\n" +
+      "Finally, don't forget about images!\n![Tux the Linux Penguin](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXiRA6gjVegGI_RxD20jPt8mf2TVFcf-nU7w&s)";
 
-//const store = createStore();
+const modifyCode = (changedCode) => {
+  return {
+    type: CODE_CHANGED,
+    changedCode
+  };
+}
 
-class AppWrapper extends React.Component {
+
+const codeReducer = (previousState, action) => {
+  switch(action.type) {
+    case CODE_CHANGED:
+      return action.changedCode;
+
+    default:
+      return previousState;
+  }
+};
+
+const store = createStore(codeReducer);
+
+export default class AppWrapper extends React.Component {
   render() {
     return (
-      <Provider>
-
+      <Provider store={store}>
+        <Container />
       </Provider>
     )
   }
 }
 
-export default class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -35,20 +66,8 @@ class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: "# This is the first header!\n" + 
-      "## This is the second header!\n" +
-      "You can create links with markdown such as this one to YouTube [Click Me!](https://www.youtube.com)\n" +
-      "This is how a line of code is made: `console.log(\'hello world\')`.\n" +
-      "The following is an inline code block:\n" +
-      "```\nfunction showcase() {\n" +
-      "    console.log(\'This is a function!\');\n" +
-      "}\n```\n\n\n" +
-      "You can create blockquotes like this: \n> Here is a blockquote\n\n" +
-      "The text can be **bold**!\n\n" +
-      "1. You can have\n2. an ordered list like this!\n\n- Or it can be\n- an unordered list instead!\n\n\n" +
-      "Finally, don't forget about images!\n![Tux the Linux Penguin](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXiRA6gjVegGI_RxD20jPt8mf2TVFcf-nU7w&s)"
+      code: defaultCode
     };
-
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -89,3 +108,19 @@ class Display extends React.Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    code: state
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    newCode: (changedCode) => {
+      dispatch(modifyCode(changedCode))
+    }
+  }
+};
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(App)
